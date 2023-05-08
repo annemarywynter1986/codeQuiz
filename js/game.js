123860623f9cef124194971f7147e4c526a1751d
annemarywynter1986 <missannemary1@gmail.com>
2023-05-08 01:29:31 -0400

    second commit with some adjustment. code is not working properly yet. saving progress

---
+++ js/game.js
@@ -0,0 +1,109 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>",
+        choice2: "<scripting>",
+        choice3: "<js>",
+        answer: "A"
+    },
+    {
+        question: "What is the correct JavaScript syntax?",
+        choice1: "#demo.innerHTML='Hello World!'",
+        choice2: "document.getElementById('p').innerHTML='Hello World!'",
+        choice3: "document.getElementById('demo').innerHTML='Hello World!'",
+        answer: "C"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        choice1: "the '<head>' section",
+        choice2: "the '<body>' section",
+        choice3: "Both the '<head>' section and the '<body>' section are correct",
+        answer: "B"
+    },
+    {
+        question: "Is JavaScript and Java same programming language",
+        choice1: "yes",
+        choice2: "no",
+        choice3: "sometimes can be considered",
+        answer: "B"
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+    
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
