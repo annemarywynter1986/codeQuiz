dacb935355c6b2a72683bbb5cb12408ae0fb67ad
annemarywynter1986 <missannemary1@gmail.com>
2023-05-09 18:22:22 -0400

    adding JQuery to add and hide buttons untill conditions are met

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,16 +2,28 @@ const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
+const playAgainBtnEl = document.querySelector('#play-again')
+const goHomeBtn = document.querySelector('#go-home')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
+$('#play-again').css("display", "none")
+
 finalScore.innerText = mostRecentScore
 
+// playAgainBtnEl.hide();
+// goHomeBtn.hide();
+
 username.addEventListener('keyup', () => {
+
     saveScoreBtn.disabled = !username.value
-})
+    displayBtns();
+   
+}
+)
+
 
 saveHighScore = e => {
     e.preventDefault()
@@ -23,7 +35,7 @@ saveHighScore = e => {
 
     highScores.push(score)
 
-    highScores.sort((a,b) => {
+    highScores.sort((a, b) => {
         return b.score - a.score
     })
 
@@ -32,5 +44,21 @@ saveHighScore = e => {
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('/')
 
-    
+
 }
+
+// saveScoreBtn.on('click', '#saveScoreBtn', function (event) {
+function displayBtns(event) {
+    if (saveScoreBtn.disabled === true) {
+        $('#play-again').css("display", "none")
+        $('#go-home').css("display", "none")
+      
+    }else {
+        $('#go-home').css("display", "block")
+        $('#go-home').css("display", "block")
+
+    }
+}
+ displayBtns();
+
+
